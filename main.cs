36f1146a3ac6224ef8d0f023a1db533c5972295a
Andy Simphaly <andytsimphaly@gmail.com>
2021-04-22 15:05:45 -0500

    Added doors to rooms

--- main.cs
+++ main.cs
@@ -107,9 +107,146 @@ public class Program {
                 r.Type = Room_Type.Maze;
         }
         // initialize doors
+        int room_num = 0;
         foreach (Room r in rooms) {
-            //do door stuff
-            
+            // Check if the room is a maze type
+            if (r.Type != Room_Type.Maze) {
+                // Check if room is start or end
+                if (r.Type != Room_Type.Start) {
+                    gen[rooms[room_num].Y,rooms[room_num].X] = 3;
+                } else if (r.Type != Room_Type.Exit) {
+                    gen[rooms[room_num].Y,rooms[room_num].X] = 4;
+                }
+                // Array to check each wall for bounds
+                bool[] walls = new bool[4];
+                // Check if a wall goes out of bounds
+                if (rooms[room_num].Y - 2 <= 0) {
+                    walls[0] = true;
+                } 
+                if (rooms[room_num].Y + r.Height + 1 >= height) {
+                    walls[1] = true;
+                }
+                if (rooms[room_num].X - 2 <= 0) {
+                    walls[2] = true;
+                }
+                if (rooms[room_num].X + r.Width + 1 >= width) {
+                    walls[3] = true;
+                }
+
+                //List of possible doors
+                var door_list = new List<List<Tuple<int, int>>>();
+                int list_num = 0;
+
+                // Loop through each wall to search for a T and put possible doors into the list
+                for (int i = 0; i < 4; i++) {
+                    bool skip_room = false;
+                    if (!walls[i]) {
+                        switch (i) {
+                            case 0:
+                                // Loop to find a T
+                                door_list.Add(new List<Tuple<int, int>>());
+                                for (int j = 0; j < r.Width; j++) {
+                                    if (gen[rooms[room_num].Y - 2,rooms[room_num].X + j] != 1) {
+                                        if (skip_room) {
+                                            list_num++;
+                                            door_list.Add(new List<Tuple<int, int>>());
+                                        }
+                                        door_list[list_num].Add(new Tuple<int, int>(rooms[room_num].X + j, rooms[room_num].Y - 1));
+                                        skip_room = false;
+                                    } else {
+                                        skip_room = true;
+                                    }
+                                }
+                                list_num++;
+                                break;
+                            case 1:
+                                door_list.Add(new List<Tuple<int, int>>());
+                                for (int j = 0; j < r.Width; j++) {
+                                    if (gen[rooms[room_num].Y + r.Height + 1,rooms[room_num].X + j] != 1) {
+                                        if (skip_room) {
+                                            list_num++;
+                                            door_list.Add(new List<Tuple<int, int>>());
+                                        }
+                                        door_list[list_num].Add(new Tuple<int, int>(rooms[room_num].X + j, rooms[room_num].Y + r.Height));
+                                        skip_room = false;
+                                    } else {
+                                        skip_room = true;
+                                    }
+                                }
+                                list_num++;
+                                break;
+                            case 2:
+                                door_list.Add(new List<Tuple<int, int>>());
+                                for (int j = 0; j < r.Height; j++) {
+                                    if (gen[rooms[room_num].Y + j,rooms[room_num].X - 2] != 1) {
+                                        if (skip_room) {
+                                            list_num++;
+                                            door_list.Add(new List<Tuple<int, int>>());
+                                        }
+                                        door_list[list_num].Add(new Tuple<int, int>(rooms[room_num].X - 1, rooms[room_num].Y + j));
+                                        skip_room = false;
+                                    } else {
+                                        skip_room = true;
+                                    }
+                                }
+                                list_num++;
+                                break;
+                            case 3:
+                                door_list.Add(new List<Tuple<int, int>>());
+                                for (int j = 0; j < r.Height; j++) {
+                                    if (gen[rooms[room_num].Y + j,rooms[room_num].X + r.Width + 1] != 1) {
+                                        if (skip_room) {
+                                            list_num++;
+                                            door_list.Add(new List<Tuple<int, int>>());
+                                        }
+                                        door_list[list_num].Add(new Tuple<int, int>(rooms[room_num].X + r.Width, rooms[room_num].Y + j));
+                                        skip_room = false;
+                                    } else {
+                                        skip_room = true;
+                                    }
+                                }
+                                list_num++;
+                                break;
+                        }
+                            
+                    }
+                }
+
+                // Generate number of doors
+                int num_doors = rand.Next(1, 3);
+
+                // Remove empty lists
+                for (int i = 0; i < door_list.Count; i++) {
+                    if (door_list[i].Count == 0) {
+                        door_list.RemoveAt(i);
+                    }
+                }
+
+                //Loop through and insert walls
+                int used_wall = -1;
+                for (int i = 0; i < num_doors; i++) {
+                    int door_coord1 = 0;
+                    // 
+                    do {
+                        door_coord1 = rand.Next(0, door_list.Count);
+                    } while (door_coord1 == used_wall);
+                    used_wall = door_coord1;
+                    int door_coord2 = rand.Next(0, door_list[door_coord1].Count);
+                    gen[door_list[door_coord1][door_coord2].Item2, door_list[door_coord1][door_coord2].Item1] = 2;
+                }
+
+                // Output to check
+                // foreach (var list in door_list) {
+                //     foreach (var tuple in list) {
+                //         Console.Write(tuple.Item1 + " " + tuple.Item2 + ", ");
+                //     }
+                //     Console.Write("  here \n" );
+                // }
+                // Console.Write(rooms[room_num].X + " " + rooms[room_num].Y +"  ------  " + r.Width + " " + r.Height + " " + num_doors + "\n");
+                
+            }
+            room_num++;
+
         }
         // initialize rooms
         foreach(Room r in rooms) {
@@ -147,16 +284,16 @@ public class Program {
         Console.WriteLine();
 
         //print triangle map
-        for(int di=1; di<width+height; di++) {
-            string temp = "";
-            for(int i = 0; i < (di<=height?height-di:di-height); i++)
-                temp += ("  ");
-            for(int ro=0; ro<(di<(height>width?width:height)?di:( di-(width>height?height:width) < Math.Abs(width-height) ? (width>height?height:width) : Math.Abs(di-width-height) )); ro++) {
-                temp += map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Left==0?"  ":map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Left+" ";
-                temp += map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Right==0?"  ":map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Right+" ";
-            }
-            Console.WriteLine(temp);
-        }
+        // for(int di=1; di<width+height; di++) {
+        //     string temp = "";
+        //     for(int i = 0; i < (di<=height?height-di:di-height); i++)
+        //         temp += ("  ");
+        //     for(int ro=0; ro<(di<(height>width?width:height)?di:( di-(width>height?height:width) < Math.Abs(width-height) ? (width>height?height:width) : Math.Abs(di-width-height) )); ro++) {
+        //         temp += map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Left==0?"  ":map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Left+" ";
+        //         temp += map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Right==0?"  ":map[(di<height?di:height)-ro-1,ro+(di>height?di-height:0)].Right+" ";
+        //     }
+        //     Console.WriteLine(temp);
+        // }
         Console.WriteLine("\nrooms: "+rooms.Count);
         
     }
